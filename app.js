f56b20932baf7c48e6211da98494464650df2e2d
claiv <claivent@gmail.com>
2025-01-29 01:06:51 +0100

    clai - 004  Last change.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Its my first AWS DevOps Project! \n I have made some changes to the code!'));
+app.get('/', (req, res) => res.send('Hello Its my first AWS DevOps Project! \n I have made some changes to the code! \n I have made some more changes to the code!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
